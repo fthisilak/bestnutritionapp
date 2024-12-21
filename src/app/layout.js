92645c1ab509cf03d41c341b762695ef54adb0be
fthisilak <fatih.isilak@virtualcontractors.io>
2024-12-21 14:35:18 +0300

    Add Netlify Identity widget for CMS integration

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@
 import './globals.css'
 import Navbar from '@/components/common/Navbar'
 import Footer from '@/components/common/Footer'
+import Script from 'next/script'
 
 export const metadata = {
   title: 'Diyetisyen Web Sitesi',
@@ -11,12 +12,31 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="tr">
+      <head>
+        <Script 
+          src="https://identity.netlify.com/v1/netlify-identity-widget.js" 
+          strategy="beforeInteractive" 
+        />
+      </head>
       <body suppressHydrationWarning className="flex flex-col min-h-screen">
         <Navbar />
         <div className="flex-grow pt-16">
           <main>{children}</main>
         </div>
         <Footer />
+        
+        {/* Identity widget için gerekli script */}
+        <Script id="netlify-identity-widget-setup">{`
+          if (window.netlifyIdentity) {
+            window.netlifyIdentity.on("init", user => {
+              if (!user) {
+                window.netlifyIdentity.on("login", () => {
+                  document.location.href = "/admin/";
+                });
+              }
+            });
+          }
+        `}</Script>
       </body>
     </html>
   )
